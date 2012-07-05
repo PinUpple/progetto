f809684f41d8eb1d2783d215620b136a92d57b67
Luca M. <luca.marzo@live.com>
2012-07-05 13:04:50 +0200

    riorganizzazione codice - classe Persona get/set

--- src/it/lab/business/core/modello/Persona.java
+++ src/it/lab/business/core/modello/Persona.java
@@ -11,8 +11,10 @@ public class Persona {
 	public String Email;
 	public String Recapito;
 	public String Ruolo;
-	
-	/** Costruttore 
+
+	/**
+	 * Costruttore
+	 * 
 	 * @param nome
 	 * @param cognome
 	 * @param username
@@ -24,8 +26,100 @@ public class Persona {
 	 * @param recapito
 	 * @param ruolo
 	 */
-	
-	public Persona(String nome, String cognome, String username, String password, String dataNascita, String citta, String indirizzo, String email, String recapito, String ruolo)
-	{ Nome = nome; Cognome = cognome; Username = username; Password = password; DataNascita = dataNascita; Citta = citta; Indirizzo = indirizzo; Email = email; Recapito = recapito; Ruolo = ruolo;}
+
+	public Persona(String nome, String cognome, String username,
+			String password, String dataNascita, String citta,
+			String indirizzo, String email, String recapito, String ruolo) {
+		Nome = nome;
+		Cognome = cognome;
+		Username = username;
+		Password = password;
+		DataNascita = dataNascita;
+		Citta = citta;
+		Indirizzo = indirizzo;
+		Email = email;
+		Recapito = recapito;
+		Ruolo = ruolo;
+	}
+
+	public String getNome() {
+		return Nome;
+	}
+
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+
+	public String getCognome() {
+		return Cognome;
+	}
+
+	public void setCognome(String cognome) {
+		Cognome = cognome;
+	}
+
+	public String getUsername() {
+		return Username;
+	}
+
+	public void setUsername(String username) {
+		Username = username;
+	}
+
+	public String getPassword() {
+		return Password;
+	}
+
+	public void setPassword(String password) {
+		Password = password;
+	}
+
+	public String getDataNascita() {
+		return DataNascita;
+	}
+
+	public void setDataNascita(String dataNascita) {
+		DataNascita = dataNascita;
+	}
+
+	public String getCitta() {
+		return Citta;
+	}
+
+	public void setCitta(String citta) {
+		Citta = citta;
+	}
+
+	public String getIndirizzo() {
+		return Indirizzo;
+	}
+
+	public void setIndirizzo(String indirizzo) {
+		Indirizzo = indirizzo;
+	}
+
+	public String getEmail() {
+		return Email;
+	}
+
+	public void setEmail(String email) {
+		Email = email;
+	}
+
+	public String getRecapito() {
+		return Recapito;
+	}
+
+	public void setRecapito(String recapito) {
+		Recapito = recapito;
+	}
+
+	public String getRuolo() {
+		return Ruolo;
+	}
+
+	public void setRuolo(String ruolo) {
+		Ruolo = ruolo;
+	}
 
 }
--- src/it/lab/business/core/modello/risorsaCartacea.java
+++ src/it/lab/business/core/modello/risorsaCartacea.java
@@ -1,27 +1,37 @@
 package it.lab.business.core.modello;
+
 /** Implementa una risorsa cartacea: libro, dispensa, ecc */
-public class risorsaCartacea extends Risorsa
-{
+public class risorsaCartacea extends Risorsa {
 
 	public String Titolo;
 	public String Autore;
 	public String Editore;
 	public String Genere;
 	public String Collocazione;
-	
-	/** Costruttore della risorsa cartacea 
+
+	/**
+	 * Costruttore della risorsa cartacea
+	 * 
 	 * @param prot
 	 * @param dataIns
 	 * @param stato
 	 * @param prestabile
 	 * @param anno
 	 * @param tipo
-	 * @param titolo	
-	 * @param autore	
+	 * @param titolo
+	 * @param autore
 	 * @param editore
 	 * @param genere
 	 * @param collocazione
 	 */
-	public risorsaCartacea(String prot, String dataIns, String stato, Boolean prestabile, Integer anno, String tipo, String titolo, String autore, String editore, String genere, String coll)
-	{	super(prot,dataIns,stato,prestabile,anno,tipo); Titolo = titolo; Autore = autore; Editore = editore; Genere = genere; Collocazione = coll; }
+	public risorsaCartacea(String prot, String dataIns, String stato,
+			Boolean prestabile, Integer anno, String tipo, String titolo,
+			String autore, String editore, String genere, String coll) {
+		super(prot, dataIns, stato, prestabile, anno, tipo);
+		Titolo = titolo;
+		Autore = autore;
+		Editore = editore;
+		Genere = genere;
+		Collocazione = coll;
+	}
 }
